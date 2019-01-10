06f09f0af74f4980db76f1cd0ecdf53a8984a87f
jonathan-gartland <jonathan.gartland@rstudio.com>
2019-01-10 15:34:29 -0500

    cleaning up local brew alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -211,6 +211,6 @@ alias dbmd='spring rake db:migrate:down'
 alias dbmu='spring rake db:migrate:up'
 
 # Homebrew
-alias brewu='brew update  && brew upgrade && brew cleanup && brew prune && brew doctor'
+alias brewu='brew update  && brew upgrade && brew cleanup && brew doctor'
 
 alias wgt='~/Projects/checkRS/shownode &'
